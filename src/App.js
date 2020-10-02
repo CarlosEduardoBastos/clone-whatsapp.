b9ab866328d940dd6fe1bc5d42507371d596f513
dudu <dudu.lima.bastos@gmail.com>
2020-10-02 09:16:39 -0300

    configurando o body e o app-window

--- src/App.js
+++ src/App.js
@@ -4,10 +4,10 @@ export default () => {
   return(
     <div className="app-window">
       <div className="sidebar">
-
+        ...
       </div>
       <div className="contentarea">
-        
+        ...
       </div>
     </div>
   );
