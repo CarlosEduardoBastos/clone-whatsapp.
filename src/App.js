e0d1ff4c21dc25c843f2088165ace4772929b339
dudu <dudu.lima.bastos@gmail.com>
2020-10-02 09:43:38 -0300

    criando e formatando a parte do sidebar e o header

--- src/App.js
+++ src/App.js
@@ -4,6 +4,21 @@ export default () => {
   return(
     <div className="app-window">
       <div className="sidebar">
+
+        <header>
+          <img className="header--avatar" src="https://sentinelasul.com.br/web/images/user_login.png" alt=""/>
+          <div className="header--buttons">
+            ...
+          </div>
+        </header>
+
+        <div className="search">
+          ...
+        </div>
+
+        <div className="chatlist">
+          ...
+        </div>
         ...
       </div>
       <div className="contentarea">
