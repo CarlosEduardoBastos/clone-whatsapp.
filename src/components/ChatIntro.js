af5f28154bc0d66759fdfbd995a73431a9aa45d2
Carlos Eduardo <dudu.lima.bastos@gmail.com>
2021-03-07 08:23:34 -0300

    concluindo a primeira parte do chat

--- src/components/ChatIntro.js
+++ src/components/ChatIntro.js
@@ -3,7 +3,7 @@ import './ChatIntro.css'
 const ChatIntro = () => {
     return (
         <div className="chatIntro">
-            <img src="" alt=""/>
+            <img src="https://web.whatsapp.com/img/intro-connection-light_c98cc75f2aa905314d74375a975d2cf2.jpg" alt=""/>
             <h1>Mantenha seu celular conectado</h1>
             <h2>O WhatsApp conecta ao seu telefone para sicronizar</h2>
         </div>
