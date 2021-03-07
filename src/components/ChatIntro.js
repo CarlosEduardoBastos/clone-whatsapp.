fccabc535d954a689835b95f3a116a38d5e3980b
Carlos Eduardo <dudu.lima.bastos@gmail.com>
2021-03-07 08:08:19 -0300

    iniciando a criação da introdução do chat

---
+++ src/components/ChatIntro.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './ChatIntro.css'
+const ChatIntro = () => {
+    return (
+        <div className="chatIntro">
+            <img src="" alt=""/>
+            <h1>Mantenha seu celular conectado</h1>
+            <h2>O WhatsApp conecta ao seu telefone para sicronizar</h2>
+        </div>
+    )
+}
+
+export default ChatIntro
